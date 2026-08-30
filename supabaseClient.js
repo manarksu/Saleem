a766324ada58bfcb9bc0526dfae58f0773e3fc8b
manarksu <443204624@student.ksu.edu.sa>
2026-08-30 12:21:59 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = "https://milvueldmckhgyjngviw.supabase.co/rest/v1/" 
+const supabaseAnonKey = "'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pbHZ1ZWxkbWNraGd5am5ndml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgwNjU2ODMsImV4cCI6MjEwMzY0MTY4M30.Dqk4fC896S1myrkwPaxERJ2-DxjzonM9qJFqU4zUNAM" 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
